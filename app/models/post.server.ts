81b2b9bf599e1b2495f0884deae80d5a3d736b4a
Richard Hounslow <rhounslow@gmail.com>
2023-04-19 11:56:49 +1000

    type safety for our posts data

---
+++ app/models/post.server.ts
@@ -0,0 +1,16 @@
+export async function getPosts() {
+
+    const posts = [
+        {
+            slug: "my-first-post",
+            title: 'My First Post',
+        },
+        {
+            slug: "my-second-post",
+            title: 'My Second Post',
+        }
+    ];
+
+    return posts;
+
+}
--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,32 +1,23 @@
 import { Link, useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
+import { getPosts } from "~/models/post.server";
+
+type LoaderData = {
+    posts: Awaited<ReturnType<typeof getPosts>>;
+};
 
 //create a async arrow fuction to fetch the posts
-export const loader = async () => {
-
-    const posts = [
-        {
-            id: 1,
-            title: 'Post 1',
-            content: 'This is the content of post 1'
-        },
-        {
-            id: 2,
-            title: 'Post 2',
-            content: 'This is the content of post 2'
-        }
-    ];
-
-
+export const loader: LoaderFunction = async () => {
+    const posts = await getPosts();
     return json({ posts })
 }
 
-
 export default function PostsRoute() {
 
-    //get the posts from the loader
-    // const { posts } = useLoaderData();
-    const posts = useLoaderData().posts;
+    // get the posts from the loader
+    const { posts } = useLoaderData() as LoaderData;
+    //const posts = useLoaderData().posts as LoaderData;
 
     //return the list of posts
     return (
@@ -34,12 +25,11 @@ export default function PostsRoute() {
             <h1>Posts</h1>
             <ul>
                 {posts.map((post) => (
-                    <li key={post.id}>
-                        <Link to={`/posts/${post.id}`}>{post.title}</Link>
+                    <li key={post.slug}>
+                        <Link to={`/posts/${post.slug}`}>{post.title}</Link>
                     </li>
                 ))}
             </ul>
         </div>
     );
-
 };
