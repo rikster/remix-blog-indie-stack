9d7f054aa791e6250a6d890ee2e5b5c3a2257801
Richard Hounslow <rhounslow@gmail.com>
2023-04-19 15:28:18 +1000

    move and rename files to follow "flat convension"
    // https://github.com/remix-run/remix/discussions/5778

---
+++ app/routes/posts.$slug.tsx
@@ -0,0 +1,13 @@
+// https://github.com/remix-run/remix/discussions/5778
+export default function PostRout() {
+
+    //return some jsx
+    return (
+        <div>
+            <h1>Post</h1>
+        </div>
+    );
+};
+
+
+
