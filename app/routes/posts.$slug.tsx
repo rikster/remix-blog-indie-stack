5c9f6fd6c0ca63135744df32643f7f619578f3f8
Richard Hounslow <rhounslow@gmail.com>
2023-04-19 17:01:56 +1000

    reduce payload size (screw graphql)

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -7,15 +7,15 @@ export const loader: LoaderFunction = async ({ params }) => {
     const { slug } = params;
     const post = await getPostBySlug(slug);
     const html = marked(post.markdown);
-    return json({ post, html }); //ensure to destructure the post object
+    return json({ title: post.title, html }); //reduce payload size 
 }
 
 export default function PostsRoute() {
-    const { post, html } = useLoaderData();
+    const { title, html } = useLoaderData();
 
     return (
         <main className="mx-auto max-w-4xl">
-            <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+            <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
             <div dangerouslySetInnerHTML={{ __html: html }} />
         </main>
     );
