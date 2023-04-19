4d6f6aa752e74a1335b27284577ccaa17de4820f
Richard Hounslow <rhounslow@gmail.com>
2023-04-19 13:48:52 +1000

    fetch only required data (so easy)

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,23 +1,23 @@
 import { Link, useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { getPosts } from "~/models/post.server";
+import { getPostListings } from "~/models/post.server";
 
 type LoaderData = {
-    posts: Awaited<ReturnType<typeof getPosts>>;
+    posts: Awaited<ReturnType<typeof getPostListings>>;
 };
 
 //create a async arrow fuction to fetch the posts
 export const loader: LoaderFunction = async () => {
-    const posts = await getPosts();
+    const posts = await getPostListings();
     return json({ posts })
 }
 
 export default function PostsRoute() {
 
     // get the posts from the loader
-    //const { posts } = useLoaderData() as LoaderData;
-    const posts = useLoaderData().posts as Awaited<ReturnType<typeof getPosts>>;
+    const { posts } = useLoaderData() as LoaderData;
+    //const posts = useLoaderData().posts as Awaited<ReturnType<typeof getPostsListings>>;
 
     //return the list of posts
     return (
